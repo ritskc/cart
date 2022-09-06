0e2fd5aa4f476e5c83f2a95b29365c501461212f
software@yellow-chips.com <software@yellow-chips.com>
2022-09-06 17:57:54 -0400

    hardcoding customer

--- cart/src/app/component/customer/customer.component.ts
+++ cart/src/app/component/customer/customer.component.ts
@@ -15,15 +15,24 @@ import { CustomerService } from 'src/app/services/customer.service';
 export class CustomerComponent implements OnInit {
   public customers: Customer[];
   public customers$ : Observable<Customer[]>;
+  public customer: Customer;
   constructor(private customerService:CustomerService) { 
     this.customers = new Array<Customer>();
     this.customers$ =  new Observable<Customer[]>();
+    this.customer = new Customer();
   }
 
   ngOnInit(): void {
     //this.customerService.getAllCustomers().subscribe(data=>{console.log(data); this.customers=data as Customer[];});
 
     this.customers$ = this.customerService.getAllCustomers();
+
+   this.customer.name="Yellow Chips";
+   this.customer.addressLine1= "23 Twilley Rd"
+   this.customer.city="Brampton";
+
+   this.customers.push(this.customer);
+    
   }
 
 }
